4299acae1c08a8db40a71df9604b4802d23a9881
Tatiana <taxtedd@gmail.com>
2024-11-12 16:54:24 +0300

    Add trigger, change models.py

--- dao/base.py
+++ dao/base.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 class BaseDAO:
     model = None
+    history_model = None
 
     @classmethod
     async def add(cls, session: AsyncSession, **values):
@@ -32,7 +33,13 @@ class BaseDAO:
     
     @classmethod
     async def get_all(cls, session: AsyncSession, **filter_by):
-      query = select(cls.model).filter_by(**filter_by)
-      result = await session.execute(query)
-      records = result.scalars().all()
-      return records
+        query = select(cls.model).filter_by(**filter_by)
+        result = await session.execute(query)
+        records = result.scalars().all()
+        return records
+    
+    @classmethod
+    async def get_by_id(cls, session: AsyncSession, id_value: Any):
+        query = select(cls.model).filter(cls.model.id == id_value)  # Предполагается, что поле id существует
+        result = await session.execute(query)
+        return result.scalar_one_or_none()
